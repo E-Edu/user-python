85f842d8374e92d483a1f326d4b0354853e8cc51
Jonas Ampferl <4706028-Ampferl@users.noreply.gitlab.com>
2020-03-21 01:00:55 +0100

    add API REST Endpoints

--- src/main.py
+++ src/main.py
@@ -7,5 +7,30 @@ def main():
     return 'Team User-Microservice'
 
 
+@app.route('/user/register', methods=['POST'])
+def user_register():
+    return 'some register shit here'
+
+
+@app.route('/user/verify', methods=['PATCH'])
+def user_verify():
+    return 'and some verify stuff here'
+
+
+@app.route('/user/login', methods=['POST'])
+def user_login():
+    return 'some login magic here'
+
+
+@app.route('/user/info', methods=['POST'])
+def user_info():
+    return 'info responses here'
+
+
+@app.route('/user/update', methods=['PUT'])
+def user_update():
+    return 'and update responses here'
+
+
 if __name__ == '__main__':
     app.run(port=4450, threaded=True)

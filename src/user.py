35637d264b4920d05c568c78e9b4c94d83efa3fd
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2020-03-21 03:32:13 +0100

    add user

---
+++ src/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, email, password, first_name, last_name, status, created, role):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.status = status
+        self.created = created
+        self.role = role

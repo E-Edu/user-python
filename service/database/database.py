46fcebc06b69cfe3e94be8dc8122c53af8f7cbff
peakstack <d4mb@protonmail.com>
2020-03-21 20:45:05 +0100

    fixed teacher token validator

--- service/database/database.py
+++ service/database/database.py
@@ -118,8 +118,19 @@ class Database():
         self.checkConnection()
         if not self.isConnected():
             return None
-        self.execute('SELECT uuid FROM User_Users WHERE email = ?', (email,))
+        self.execute('SELECT uuid FROM User_Users WHERE email = ?', email)
         res = self.cursor.fetchone()
         if res == None:
             return None
         return res[0]
+
+
+    def getTeacherTokenExisting(self, teacher_token):
+        self.checkConnection()
+        if not self.isConnected():
+            return None
+        self.execute('SELECT COUNT(token) FROM User_Teacher WHERE token = ?', teacher_token)
+        res = self.cursor.fetchone()
+        if res is None:
+            return None
+        return res[0] > 0

ef1f0a019e49427582643fb4978ef87c61f69fef
lokmeinmatz <matze.kind@web.de>
2020-03-21 15:59:33 +0100

    added case if user wasn't provided

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 from errorResponse import ErrorResponse
 from userRegistrar import UserRegistrar
 import database.database as db
+import jwt
 
 
 app = Flask(__name__)
@@ -37,8 +38,16 @@ def user_login():
 
 @app.route('/user/info', methods=['POST'])
 def user_info():
+    """
+    session: JWT as string
+    user: optional, mail address of other user you want to get information about
+    """
+
+    # TODO: Verify that the request is valid (from session)
+
     global database
     req_body = None
+
     try:
         req_body = json.loads(request.data)
     except ValueError:
@@ -49,27 +58,31 @@ def user_info():
         error = ErrorResponse('No session provided', 400)
         return jsonify(error.get_description()), error.get_code()
 
+    user_id_to_query = None
+    # switch based on wheather the user was provided
     if (not 'user' in req_body) or req_body['user'] == None:
         # return information about owner of this session
-        # TODO: fetch user info from session id
-        pass
+        # TODO: where in the JWT is the id stored?
+        token_payload = jwt.decode(req_body.session)
+        user_id_to_query = token_payload['id'] # TODO put here the correct field name
     else:
         # return informatin about the provided user
         user_to_query = req_body['user']
-        if not utils.is_mail_format(user_to_query) or not database.existsUser(user_to_query):
+        if not user_registrar._is_valid_email(user_to_query) or not database.existsUser(user_to_query):
             error = ErrorResponse('No valid user', 400)
             return jsonify(error.get_description()), error.get_code()
 
-        user_id = database.getUserIdByEmail(user_to_query) # user exists after check above
-        infos = database.getUserInfo(user_id)
+        user_id_to_query = database.getUserIdByEmail(user_to_query) # user exists after check above
+    
+    infos = database.getUserInfo(user_id)
 
-        # TODO: privileged student + report_spammer
-        return {
-            'teacher': infos[7] == db.Role.TEACHER,
-            'admin': infos[7] == db.Role.ADMIN,
-            'priviliged_student': False,
-            'report_spammer': 0
-        }
+    # TODO: privileged student + report_spammer
+    return {
+        'teacher': infos[7] == db.Role.TEACHER,
+        'admin': infos[7] == db.Role.ADMIN,
+        'priviliged_student': False,
+        'report_spammer': 0
+    }
 
 
 @app.route('/user', methods=['PUT'])

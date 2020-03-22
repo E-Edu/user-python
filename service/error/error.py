a87fd3407c58404be949a981fe33d442e82e798f
Ampferl <Jonas@ampferl.com>
2020-03-22 15:23:58 +0100

    Add ban usecase

--- service/error/error.py
+++ service/error/error.py
@@ -13,6 +13,11 @@ class InfoErrorUserNotFound(InfoError):
         super().__init__("User not found")
 
 
+class BanErrorUserNotFound(InfoError):
+    def __init__(self):
+        super().__init__("User not found")
+
+
 class LoginError(Error):
     def __init__(self, message):
         super().__init__(message)

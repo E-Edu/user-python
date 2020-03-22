6cef8226502ea8c74f9777a6d2142c7af6926163
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2020-03-22 02:24:36 +0100

    info usecase

--- service/usecases/info.py
+++ service/usecases/info.py
@@ -1,10 +1,11 @@
-from service.transfer.output import *
+from service.transfer.output import Info as InfoOut
+from service.transfer.input import Info as InfoIn
 from service.repository.user import *
 from service.error.error import *
 
 
-def info(user_uuid):
-    user = get_user(user_uuid)
+def info(input: InfoIn):
+    user = get_user(input.uuid)
     if user is None:
         return Error("User not found")
-    return User(user)
+    return InfoOut(user)

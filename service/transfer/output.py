e60e7825339c16a26028110e3c8f9fec661af2f4
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2020-03-22 03:03:51 +0100

    update routes

--- service/transfer/output.py
+++ service/transfer/output.py
@@ -2,6 +2,7 @@ from service.user import *
 from service.status import *
 from service.role import *
 
+
 class Login:
     def __init__(self, session):
         self.session = session
@@ -16,7 +17,7 @@ class Info:
     def __init__(self, user: User):
         self.teacher = user.role == Role.TEACHER,
         self.admin = user.role == Role.ADMIN,
-        self.priviliged_student = user.role == Role.PRIVILIGED_STUDENT,
+        self.priviliged_student = user.role == Role.PRIVILEGED_STUDENT,
         self.report_spammer = user.status == Status.REPORTED
 
 
